9be3823936b8ec0ac83bc535585c258bb5b06a1e
NguyenDoTanKhoa <dragonball1072003@gmail.com>
2024-02-24 09:46:37 +0700

    init project

---
+++ WebTraSua/Areas/Admin/Controllers/LienHeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTraSua.Areas.Admin.Controllers
+{
+    public class LienHeController : Controller
+    {
+        // GET: Admin/LienHe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
